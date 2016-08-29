0be2485a757bc65023ba9736f37562e03e5acb56
hlv68 <hlv68@mail.missouri.edu>
2016-08-28 21:44:06 -0500

    hlv68

---
+++ story.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include "sentences/_HEADERS.h"
+
+// I assume all of you know C.
+
+int main( void )
+{
+	printf("\n\n");
+	bensSentence1();
+	printf("\n\n");
+
+	// Add your sentence.
+	// Don't forget to update the header file.
+	// vvvvvvvvvvvvvvvv STUDENT WORK HERE
+
+//	wbSentence1();
+//	wbSentence2();
+
+    btkvfSentence2();
+
+    yisSentence1();
+	jdhcp3Sentence1();
+	yisSentence2();
+	jdhcp3Sentence2();
+	lxj_sentence1();
+	lxj_sentence2();
+
+//	awwxbbSentence1();
+	wb_MySentence1();
+	wb_MySentence2();
+//	awwxbbSentence_FINAL();
+	matthewsSentence1();
+
+	matthewsSentence2();
+
+    KurtsSentence1();
+
+	kcfk28_sentence1();
+
+	holtSentence1();
+
+    KurtsSentence_FINAL();
+
+	wb_MySentence1();
+	wb_MySentence2();
+    
+	    
+	    
+	wb_MySentence2();
+
+	aliSentence1();
+	aliSentence2();
+
+	tiit37Sentence1();
+	drd436Sentence1();
+	tiit37Sentence2();
+	drd436Sentence2();
+    willsSentence();
+    cakd47Sentence1();
+    cakd47Sentence2();
+    
+    //swof44Sentence1();
+    //swof44Sentence2();
+
+    jwSentence1();
+	jwSentence2();
+
+	holtSentence2();
+
+	kienanSentence1();
+	kienanSentence2();
+    
+    	hlv68Sentence1();
+    	hlv68Sentence2();
+
+	WXLSentence1();
+	WXLSentence2();
+
+	kcfk28_sentence2();
+
+    tpwvycSentence1();
+    tpwvycSentence2();
+
+    jry83Sentence1();
+    jry83Sentence2();
+    btkvfSentence1();
+    willSentenceLast();
+
+
+	// DO NOT ADD AFTER THIS COMMENT
+	// Bens sentence should be last.
+	bensSentence_FINAL();
+	printf("\n\n");
+}
